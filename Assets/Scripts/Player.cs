9cfbdecbf2bde6cf8e7c67469afa80bdd1a1c5aa
Jerome MR <10962717+Assasquid@users.noreply.github.com>
2023-07-15 21:21:59 +0100

    Add player movement.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 8f;
+
     private void Update()
     {
+        Vector2 inputVector = new Vector2(0, 0);
         if (Input.GetKey(KeyCode.W))
         {
-            Debug.Log("Pressing!");
+            inputVector.y = +1;
         }
-        else
+        if (Input.GetKey(KeyCode.S))
         {
-            Debug.Log("-");
+            inputVector.y = -1;
         }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
+
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        Debug.Log(inputVector);
     }
 }
